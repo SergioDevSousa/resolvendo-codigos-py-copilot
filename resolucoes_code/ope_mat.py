a7c9b37b099169b2a6a3cd499655d2b86e289aa7
DEVserjao <166451865+SergioDevSousa@users.noreply.github.com>
2024-12-09 16:07:23 +0000

    codigo inicial desafio

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,23 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+
+operacao = input("Escolha uma operação(+, -, *, /): ")
+
+if operacao == '+':
+    resultado = numero1 + numero2
+elif operacao == '-':
+    resultado = numero1 - numero2
+elif operacao == '*':
+    resultado = numero1 - numero2
+elif operacao == '/':
+    if numero2 != 0:
+        resultado = numero1 / numero2
+    else:
+        resultado = "Erro: Divisão por zero não é permitida!"
+else:
+    resultado = "Operação inválida!"
+
+print(f"Resultado: {resultado}")
+
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+# utilizar o método join para poder separar as strings repetidas
+resultado = " ".join([string] * numero)
+print(resultado)
